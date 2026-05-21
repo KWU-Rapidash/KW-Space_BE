a763054b97dfa8cc1e44af25e8d23e72a8376332
keonha <keonha@DESKTOP-FI22N22>
2026-05-21 19:26:27 +0900

    feat: :tada: spring 프로젝트 생성
    #1

---
+++ src/main/java/com/example/KW_SPACE/KwSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.KW_SPACE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwSpaceApplication.class, args);
+	}
+
+}
